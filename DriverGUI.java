9c214a65f534e2bfe83fbadc9e9d1307da7d55d5
FGNINE9 <f.garcia777@icloud.com>
2024-07-29 16:27:06 -0700

    -Added javaFX library -Updated Main to DriverGUI wich is the General User Interface for a Driver

---
+++ DriverGUI.java
@@ -0,0 +1,60 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+
+public class DriverGUI extends Application {
+
+
+    @Override
+    public void start(Stage Stage){
+
+        Stage.setMaximized(true);
+        Stage.show();
+        double Ywin = Stage.getY();
+        double Xwin = Stage.getX();
+        String Name = "";
+
+
+        //Labels
+        Label title = new Label("Welcome To fleet Manager" + Name);
+        title.setTranslateY(-Ywin/3);
+
+        // Buttons
+        Button Upload = new Button("Upload");
+        Upload.setTranslateX(-Xwin/4);
+
+        //Images
+        Image icon = new Image("Icon.png");
+
+        //Layout
+        StackPane root = new StackPane();
+        root.getChildren().add(title);
+        root.getChildren().add(Upload);
+
+        //Scenes
+        Scene scene = new Scene(root);
+
+        //Primary Stage
+        Stage.getIcons().add(icon);
+        Stage.setScene(scene);
+        Stage.setTitle("Fleet Management System");
+    }
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+
+    }
+
+    public static String getName(int ID){
+
+        return "None";
+    }
+
+}
