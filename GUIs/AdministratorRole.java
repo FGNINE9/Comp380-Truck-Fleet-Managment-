ca40c403cb85b0f01a1ed9ffc3d0b45730c3cee0
BiskySheed <raashidus@gmail.com>
2024-08-06 15:51:36 -0700

    Adding Ticket Class

--- GUIs/AdministratorRole.java
+++ GUIs/AdministratorRole.java
@@ -24,53 +24,48 @@ public class AdministratorRole
     }
 
     public void EditEmployeeData(){
-    	JobDatabase database = new JobDatabase();
-    	String command = "";
-    	
+        JobDatabase database = new JobDatabase();
+        String command = "Yes";
+
         System.out.println("Printing Employee Data...");
         database.printDatabase();
         System.out.println("Select Yes to Edit");
         System.out.println("Select No to Exit ");
-        if (command == "Yes") {
-        	System.out.println("Select fields to edit ");
-        	// edit specific employee fields
+        if ("Yes".equals(command)) {
+            System.out.println("Select fields to edit ");
+            // edit specific employee fields
         } else {
-        	System.out.println("Exiting Menu ");
+            System.out.println("Exiting Menu ");
         }
-        
+
     }
     public void AssignJobs(){
-    	Job job = new Job();
-    	String command = "";
-    	
+        Job job = new Job();
+        String command = "Yes";
+
         System.out.println("Assign GUIs.Job to Employee...");
         job.printJob();
         System.out.println("Please Select Options for job");
-        if (command == "Yes") {
-        	job.logJob();  
+        if ("Yes".equals(command)) {
+            job.logJob();
         } else {
-        	System.out.println("Exit log");
+            System.out.println("Exit log");
         }
-        
+
     }
     public void ViewTickets() {
-        String command = "";
+        Tickets ticket = new Tickets();
+        String command = "Yes";
         System.out.println("Current ticket ");
         //Display Ticket information and then prompt user to download
-        //.printTicket();
+        ticket.PrintTicket();
         System.out.println("If you would like to download the Ticket enter DownloadTickets");
-        if (command == "Save"){
-            DownloadTickets();
+        if ("Save".equals(command)){
+            ticket.DownloadTickets();
+        } else {
+            System.out.println("Exit");
         }
 
     }
-    public void DownloadTickets(){
-        //Function to export ticket file
-        System.out.println("Download ticket");
-        //create ticket file
-    }
-
-
-
 
 }
