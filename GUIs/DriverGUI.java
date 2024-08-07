385f7e51556146db7fc766c01803d17f2c9b7f45
FGNINE9 <f.garcia777@icloud.com>
2024-08-07 02:27:35 -0700

    IMporved job GUI

--- GUIs/DriverGUI.java
+++ GUIs/DriverGUI.java
@@ -23,7 +23,7 @@ public class DriverGUI extends Application {
 
         Job GetJob = new Job();
 
-        Stage.show();
+
         double Xwin = 800;
         double Ywin = 600;
         String Jobs = GetJob.printJob();
@@ -48,26 +48,30 @@ public class DriverGUI extends Application {
         root.getChildren().add(ReturnB);
 
         TextArea textBox= new TextArea();
+        textBox.setPrefSize(175, 175);
         textBox.setText(Jobs);
         textBox.setEditable(false);
+        textBox.setWrapText(true);
+        textBox.setTranslateX(Xwin/45);
+        textBox.setTranslateY(Ywin/9.5);
 
         ScrollPane scroll = new ScrollPane();
-        scroll.setTranslateX(Xwin/100);
-        scroll.setTranslateY(Ywin/100);
-        scroll.setPrefViewportHeight(Xwin/2.3);
-        scroll.setPannable(true);
+
         scroll.setContent(textBox);
         root.getChildren().add(scroll);
 
         //Scenes
         Scene scene = new Scene(root, Xwin, Ywin);
 
+        title.toFront();
+        ReturnB.toFront();
 
         //Primary Stage
         Stage.setResizable(false);
         Stage.getIcons().add(icon);
         Stage.setScene(scene);
         Stage.setTitle("Fleet Management System");
+        Stage.show();
 
         ReturnB.setOnAction(new EventHandler<ActionEvent>()
         {
